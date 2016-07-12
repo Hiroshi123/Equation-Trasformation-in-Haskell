6d2b775c110e1a87617c313d6cc3a844cbf4a4eb
hiroshi <vrodxda@gmail.com>
2016-07-12 18:44:05 +0200

    main loop added

---
+++ operation.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Operation
+  (
+    OperationClass(..) 
+  ) where
+
+import Math1
+
+class OperationClass where
+  transpose :: Tree Item -> Tree Item
+  transpose' :: Tree Item -> (Double,Tree Item)
+  transpose'' :: Double -> Tree Item -> Tree Item
+
+  zeroRemove :: Tree Item -> Tree Item
+  stopCalc :: Tree Item -> Bool
+  
+  calc :: Tree Item -> Tree Item
+  
+  opeAdd :: Tree Item -> Tree Item -> Tree Item
+  opeSub :: Tree Item -> Tree Item -> (Tree Item,Maybe Int)  
+  
+instance OperationClass where
+  transpose a = do
+    let (left,right)  = lrSep a
+    let (b,c) = transpose' left
+    --let e     = zeroRemove $ calc c
+    N Equ c (transpose'' b right)
+    
+  transpose' (N c (L (Num a)) (L (Var b))) = (invSig a, (N c (N Add (L (Num a)) (L (Num (invSig a)))) (L (Var b)) ) )
+  transpose' (N c (L (Var b)) (L (Num a))) = (invSig a, (N c (L (Var b)) (N Add (L (Num a)) (L (Num (invSig a))) ) ) )
+  transpose' a = (0.0, a)
+
+  transpose'' s (N c (L (Num a)) b) = (N c (N Add (L (Num a)) (L (Num s) ) )  b )
+  transpose'' s a = a
+  
+  zeroRemove (N c (L (Num 0.0)) (L (Var b))) = (L (Var b))
+  zeroRemove (N c (L (Var b)) (L (Num 0.0))) = (L (Var b))
+  zeroRemove a = a
+
+
+  stopCalc (N a (L (Num b)) (L (Var c))) = True
+  stopCalc (N a (L (Var b)) (L (Num c))) = True
+  stopCalc (L (Var b)) = True
+  stopCalc (L (Num c)) = True
+  stopCalc a = False
+  
+  calc (N Add (L (Num a)) (L (Num b))) = L (Num (a + b))
+  calc (N Sub (L (Num a)) (L (Num b))) = L (Num (a - b))
+
+  calc (N Add (N Add (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Add (L (Var v1)) (N Add (L (Num n1)) (L (Num n2)) )
+  calc (N Add (N Add (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Add (N Add (L (Num n1)) (L (Num n2))) (L (Var v1) )
+
+  calc (N Add (N Sub (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Add (L (Var v1)) (N Sub (L (Num n2)) (L (Num n1)) )
+  calc (N Add (N Sub (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Add (N Sub (L (Num n1)) (L (Num n2))) (L (Var v1) )
+
+  calc (N Sub (N Add (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Add (L (Var v1)) (N Sub (L (Num n1)) (L (Num n2)) )
+  calc (N Sub (N Add (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Add (N Sub (L (Num n1)) (L (Num n2))) (L (Var v1) )
+
+  calc (N Sub (N Sub (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Sub (L (Var v1)) (N Add (L (Num n1)) (L (Num n2)) )
+  calc (N Sub (N Sub (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Sub (N Sub (L (Num n1)) (L (Num n2))) (L (Var v1) )
+
+  --
+
+  calc (N Add (L (Num n1)) (N Add (L (Num n2)) (L (Var v1)) ) ) = N Add (N Add (L (Num n1)) (L (Num n2)) ) (L (Var v1))
+  calc (N Add (L (Num n1)) (N Add (L (Num n2)) (L (Var v1)) ) ) = N Add (L (Var v1)) (N Add (L (Num n1)) (L (Num n2)) )
+
+
+  calc (N Add (N Sub (L (Num n1)) (L (Num n2))) (L (Var v1)) ) = N Sub (N Add (L (Num n1)) (L (Num n2))) (L (Var v1) )
+  calc (N Add (N Sub (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Add (L (Var v1)) (N Sub (L (Num n1)) (L (Num n2)))
+
+  calc (N Sub (L (Num n1)) (N Add (L (Num n2)) (L (Var v1)) ) ) = N Sub (N Sub (L (Num n1)) (L (Num n2)) ) (L (Var v1))
+  calc (N Sub (L (Num n1)) (N Add (L (Num n2)) (L (Var v1)) ) ) = N Sub (N Sub (L (Num n1)) (L (Num n2)) ) (L (Var v1))
+
+  calc (N Sub (L (Num n1)) (N Sub (L (Num n2)) (L (Var v1)) ) ) = N Add (N Sub (L (Num n1)) (L (Num n2)) ) (L (Var v1))
+  calc (N Sub (L (Num n1)) (N Sub (L (Num n2)) (L (Var v1)) ) ) = N Sub (N Add (L (Num n1)) (L (Num n2)) ) (L (Var v1))
+  
+  calc (N a b c) = (N a (calc b) (calc c))
+  calc (L a) = L a
+  
+  opeAdd (L (Num a)) (L (Num b)) = L ( Num (a + b) )
+  --opeAdd c d = (c,Nothing)
+  
+  opeSub (L (Num a)) (L (Num b)) = (L ( Num (a - b) ),Just 1)
+  opeSub c d = (c,Nothing)
+  
+  
--- playGround1.hs
+++ playGround1.hs
@@ -1,11 +1,33 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+import Mainloop
 
-import Math1
 
 main :: IO ()
 main = do
-  print $ process' "(+ (+ X (- 1 19)) 1) = (- 4 5)"
+  
+  loop
+
+  
+  --loop
+  --let a = serialize $ toTree $ read_expr "= (+ (+ X (- 1 19)) 1) (+ (- 4 5) 32)"
+  --print a
+  --print $ toString' a
+
+--  let a = process' " = (+ (+ X (- 1 19)) 1) (- 4 5)"
+--  print a 
+--  let b = transpose (N Equ (N Add (L (Num 1.0)) (L (Var "X")) ) (N Add (L (Num 4.0)) (L (Num (-5.0)) )) )
+--  print b
+
+{--
+  let a = toTree $ read_expr "= (+ (+ X (- 1 19)) 1) (- 4 5)"
+  print a
+  let (b,s) = as' a
+  print b
+  let c = calc b
+  print c
+  let d = calc c
+  print d
+  let e = calc d
+  print e
+--}
+
   print 10
-
-
-
