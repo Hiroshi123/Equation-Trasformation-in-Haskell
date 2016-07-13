0dbd445ef9c6a35f4252f0ba821951519ba6ef5a
hiroshi <vrodxda@gmail.com>
2016-07-13 16:17:16 +0200

    basic calculation complete

--- operation.hs
+++ operation.hs
@@ -2,16 +2,18 @@
 
 module Operation
   (
-    OperationClass(..) 
+    OperationClass(..)
   ) where
+
 
 import Math1
 
 class OperationClass where
-  transpose :: Tree Item -> Tree Item
-  transpose' :: Tree Item -> (Double,Tree Item)
-  transpose'' :: Double -> Tree Item -> Tree Item
-
+  transpose   :: Tree Item -> Tree Item
+  transpose'  :: Tree Item -> (String,Double,Tree Item)
+  transpose'' :: String    -> Double -> Tree Item -> Tree Item
+  
+  
   zeroRemove :: Tree Item -> Tree Item
   stopCalc :: Tree Item -> Bool
   
@@ -23,83 +25,68 @@ class OperationClass where
 instance OperationClass where
   transpose a = do
     let (left,right)  = lrSep a
-    let (b,c) = transpose' left
+    let (str,b,c) = transpose' left
     --let e     = zeroRemove $ calc c
-    N Equ c (transpose'' b right)
-    
-  transpose' (N c (L (Num a)) (L (Var b))) = (invSig a, (N c (N Add (L (Num a)) (L (Num (invSig a)))) (L (Var b)) ) )
-  transpose' (N c (L (Var b)) (L (Num a))) = (invSig a, (N c (L (Var b)) (N Add (L (Num a)) (L (Num (invSig a))) ) ) )
-  transpose' a = (0.0, a)
-
-  transpose'' s (N c (L (Num a)) b) = (N c (N Add (L (Num a)) (L (Num s) ) )  b )
-  transpose'' s a = a
+    N Equ c (transpose'' str b right)
   
-  zeroRemove (N c (L (Num 0.0)) (L (Var b))) = (L (Var b))
-  zeroRemove (N c (L (Var b)) (L (Num 0.0))) = (L (Var b))
+  transpose' (N Add (L (Num a)) b) = ("Add",invSig a, (N Add (N Add (L (Num a)) (L (Num (invSig a)))) b ) )
+  transpose' (N Mul (L (Num a)) (L (Var b))) =  ("Div",a, (N Mul (N Div (L (Num a)) (L (Num a)) ) (L (Var b)) ) )
+  
+  transpose' a = ("Add",0.0, a)
+  
+  transpose'' "Add" s b = N Add (L (Num s)) b
+  transpose'' "Div" s b = N Div (L (Num s)) b
+  
+  transpose'' "Add" s a = a
+  
+  zeroRemove (N Add (L (Num 0.0)) b) = b
+  zeroRemove (N Mul (L (Num 1.0)) b) = b
   zeroRemove a = a
-
-
+  
   stopCalc (N a (L (Num b)) (L (Var c))) = True
-  stopCalc (N a (L (Var b)) (L (Num c))) = True
+  stopCalc (N a (L (Num b)) (N Mul (L (Num c)) (L (Var d)) )) = True
+  
   stopCalc (L (Var b)) = True
   stopCalc (L (Num c)) = True
   stopCalc a = False
   
+  --basic operation <priority number1>
   calc (N Add (L (Num a)) (L (Num b))) = L (Num (a + b))
   calc (N Sub (L (Num a)) (L (Num b))) = L (Num (a - b))
   calc (N Mul (L (Num a)) (L (Num b))) = L (Num (a * b))
   calc (N Div (L (Num a)) (L (Num b))) = L (Num (a / b))
-
-  --left 2 :  right : 1 (Add:Sub)
   
-  calc (N Add (N Add (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Add (L (Var v1)) (N Add (L (Num n1)) (L (Num n2)) )
-  calc (N Add (N Add (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Add (N Add (L (Num n1)) (L (Num n2))) (L (Var v1) )
+  --inversion ( sbbtraction -> addition ) <commutativity>
+  --e.g. (x-5) -> (x+(-5))
+  calc (N Sub a (L (Num n1)) ) = (N Add a (L (Num (invSig n1))) )
   
-  calc (N Add (N Sub (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Add (L (Var v1)) (N Sub (L (Num n2)) (L (Num n1)) )
-  calc (N Add (N Sub (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Add (N Sub (L (Num n1)) (L (Num n2))) (L (Var v1) )
+  --e.g. (5-x) -> (5+(-x))
+  calc (N Sub a (L (Var v1)) ) = (N Add a (L (Var (invSigC v1))) )
   
-  calc (N Sub (N Add (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Add (L (Var v1)) (N Sub (L (Num n1)) (L (Num n2)) )
-  calc (N Sub (N Add (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Add (N Sub (L (Num n1)) (L (Num n2))) (L (Var v1) )
+  --addition <commutativity>
+  --e.g. (x+5) -> (5+x)
+  calc (N Add a (L (Num n1))) = (N Add (L (Num n1)) a )
   
-  calc (N Sub (N Sub (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Sub (L (Var v1)) (N Add (L (Num n1)) (L (Num n2)) )
-  calc (N Sub (N Sub (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Sub (N Sub (L (Num n1)) (L (Num n2))) (L (Var v1) )
+  --multiplication <commutativity>
+  --e.g. (x*5) -> (5*x)
+  calc (N Mul a (L (Num n1))) = (N Mul (L (Num n1)) a )
   
-  --left 1 :  right : 2 (Add:Sub)
+  --(addition,addition) <associativity>
+  --e.g. 2+(5+x) -> (2+5)+x
+  calc (N Add (L (Num n1)) (N Add (L (Num n2)) a ) ) = N Add (N Add (L (Num n1)) (L (Num n2))) a
   
-  calc (N Add (L (Num n1)) (N Add (L (Num n2)) (L (Var v1)) ) ) = N Add (N Add (L (Num n1)) (L (Num n2)) ) (L (Var v1))
-  calc (N Add (L (Num n1)) (N Add (L (Num n2)) (L (Var v1)) ) ) = N Add (L (Var v1)) (N Add (L (Num n1)) (L (Num n2)) )
+  --(multiplication,mulitplication) <associativity>
+  --e.g. 3*(5*x) -> (3*5)*x
+  calc (N Mul (L (Num n1)) (N Mul (L (Num n2)) a ) ) = N Mul (N Mul (L (Num n1)) (L (Num n2))) a
   
-  calc (N Add (N Sub (L (Num n1)) (L (Num n2))) (L (Var v1)) ) = N Sub (N Add (L (Num n1)) (L (Num n2))) (L (Var v1) )
-  calc (N Add (N Sub (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Add (L (Var v1)) (N Sub (L (Num n1)) (L (Num n2)))
-  
-  calc (N Sub (L (Num n1)) (N Add (L (Num n2)) (L (Var v1)) ) ) = N Sub (N Sub (L (Num n1)) (L (Num n2)) ) (L (Var v1))
-  calc (N Sub (L (Num n1)) (N Add (L (Num n2)) (L (Var v1)) ) ) = N Sub (N Sub (L (Num n1)) (L (Num n2)) ) (L (Var v1))
-  
-  calc (N Sub (L (Num n1)) (N Sub (L (Num n2)) (L (Var v1)) ) ) = N Add (N Sub (L (Num n1)) (L (Num n2)) ) (L (Var v1))
-  calc (N Sub (L (Num n1)) (N Sub (L (Num n2)) (L (Var v1)) ) ) = N Sub (N Add (L (Num n1)) (L (Num n2)) ) (L (Var v1))
-  
-  
-  --left 2 :  right : 1 (Add:Mul)
-  
-  calc (N Mul (N Mul (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Mul (L (Var v1)) (N Mul (L (Num n1)) (L (Num n2)) )
-  calc (N Mul (N Mul (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Mul (N Mul (L (Num n1)) (L (Num n2))) (L (Var v1) )
-  
-  calc (N Mul (N Add (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Add (N Mul (L (Num n2)) (L (Var v1)) ) (N Mul (L (Num n1)) (L (Num n2)) )
-  calc (N Mul (N Add (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Add (N Mul (L (Num n1)) (L (Num n2)) ) (N Mul (L (Num n2)) (L (Var v1)) )
-  
-  
---  calc (N Add (N Mul (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Mul (L (Add v1)) (N Mul (L (Num n1)) (L (Num n2)) )
---  calc (N Add (N Mul (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Mul (N Add (L (Num n1)) (L (Num n2))) (L (Var v1) )
-
---  calc (N Mul (N Mul (L (Var v1)) (L (Num n1))) (L (Num n2)) ) = N Mul (L (Mul v1)) (N Mul (L (Num n1)) (L (Num n2)) )
---  calc (N Mul (N Mul (L (Num n1)) (L (Var v1))) (L (Num n2)) ) = N Mul (N Mul (L (Num n1)) (L (Num n2))) (L (Var v1) )
+  --(multiplication,addition) <distribution>
+  --e.g. 2*(2+x)-> 2*2 + 2*x
+  calc (N Mul (L (Num n1)) (N Add (L (Num n2)) a ) ) = N Add (N Mul (L (Num n1)) (L (Num n2)) ) (N Mul (L (Num n1)) a )
   
   calc (N a b c) = (N a (calc b) (calc c))
   calc (L a) = L a
   
   opeAdd (L (Num a)) (L (Num b)) = L ( Num (a + b) )
-
-  
   --opeAdd c d = (c,Nothing)
   
   opeSub (L (Num a)) (L (Num b)) = (L ( Num (a - b) ),Just 1)
